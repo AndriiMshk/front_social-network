e1c8df29e6d721dd8514fb6f231b70bab71baa5e
AndrMshk <andrmshk1@gmail.com>
2022-04-16 14:42:39 +0300

    lesson 30 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import {Header} from './components/Header/Header';
-import {Navbar} from './components/Navbar/Navbar';
-import {Profile} from './components/Profile/Profile';
-import {Dialogs} from "./components/Dialogs/Dialogs";
-import {News} from "./components/News/News";
-import {Music} from "./components/Music/Music";
-import {Settings} from "./components/Settings/Settings";
-import {stateType} from './Redux/store'
 
-type appPropsType = {
-    state: stateType
-}
-
-const App: React.FC<appPropsType> = (props) => {
-    return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar
-                    navbarState={props.state.sidebar}
-                />
-                <div className='app-wrapper-content'>
-                    <Route path={'/profile'} render={() =>
-                        <Profile
-                            postsState={props.state.profile.postsData}
-                        />
-                    }/>
-                    <Route path={'/dialogs'} render={() =>
-                        <Dialogs
-                            dialogsState={props.state.dialogs}
-                        />
-                    }/>
-                    <Route path={'/news'} component={News}/>
-                    <Route path={'/music'} component={Music}/>
-                    <Route path={'/settings'} component={Settings}/>
-                </div>
-            </div>
-        </BrowserRouter>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
